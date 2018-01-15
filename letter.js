3cd3906ba8830134fa80f0704de43aa665e82850
Peter Burns <peterburnshicks@gmail.com>
2018-01-14 23:59:19 -0500

    added hangman, word and letter js files

---
+++ letter.js
@@ -0,0 +1,8 @@
+function Letter (letter){
+    this.guessed = false;
+    this.letter = letter;
+}
+Letter.prototype.display =  function (){
+    return this.guessed ? this.letter : '_';
+};
+module.exports = Letter;
